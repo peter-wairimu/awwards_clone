bec4583e01af33ac32a514f4600dcc2a4eea6bcb
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-20 22:07:16 +0300

    added profile to views.py

--- awwards/views.py
+++ awwards/views.py
@@ -60,4 +60,29 @@ def userPage(request):
 
 
 
+def profile(request):
+    user = request.user
+    user = Profile.objects.get_or_create(user= request.user)
+    
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)                         
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated successfully!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+        'user': user
+
+    }
+
+    return render(request, 'accounts/profile.html', context)
 
