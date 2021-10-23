ce3ce37b30539874c6375c6f0d9cf109cda42353
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-23 14:22:30 +0300

    added a project class in models.py

--- awwards/models.py
+++ awwards/models.py
@@ -8,3 +8,16 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     image = models.ImageField(upload_to ='profile_pics')
+
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=50, blank=True)
+    project_photo = models.ImageField(upload_to='pro_image')
+    description = models.TextField(max_length=600, blank=True)
+    github_repo = models.CharField(max_length=200, blank=True)
+    url = models.CharField(max_length=50, blank=True)
+    owner = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.url
