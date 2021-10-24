414c454671d697b5b6b66a16a9c70d8f684d28f4
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-25 00:17:22 +0300

    udated settings.py

--- projectaward/settings.py
+++ projectaward/settings.py
@@ -129,10 +129,26 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
 ]
 
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'peters-gallery',
+    'API_KEY': '825258561278761',
+    'API_SECRET': '8yYvkIlIcJIKrpO40NG2DbaiUGE'
+}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+django_heroku.settings(locals())
