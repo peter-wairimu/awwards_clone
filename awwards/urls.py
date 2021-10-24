40d486ab10d208fd73535a3e55918fd431252a01
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-25 00:11:22 +0300

    updated urls.py

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns =[
 
@@ -17,3 +20,8 @@ urlpatterns =[
    
 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    
