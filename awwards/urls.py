83001dfba2810bee29369a88f8eb2a6c2a51c493
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-24 09:27:17 +0300

    added search url and updated the navbar

--- awwards/urls.py
+++ awwards/urls.py
@@ -12,6 +12,7 @@ urlpatterns =[
     path('',views.home, name="home"),
     path('project_details/<int:id>/', views.detail, name="project_details"),
     path('add_project/',views.add_project , name="add_project"),
+    path('search_results/',views.search_results, name='search_results'),
    
 
 ]
